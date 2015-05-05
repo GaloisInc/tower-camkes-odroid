3340ef6b926e1d9186be00ecd530ad265c9ffedc
leepike <leepike@gmail.com>
2015-05-05 16:06:42 -0700

    serial: test app.

--- src/Tower/Odroid/Serial.hs
+++ src/Tower/Odroid/Serial.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -11,50 +10,59 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 --------------------------------------------------------------------------------
--- UART Client.
+-- UART interface.
 --
 -- (c) 2015 Galois, Inc.
 --
 --------------------------------------------------------------------------------
 
 module Tower.Odroid.Serial
-  ( uartMonitor
+  ( uartTower
   , uart_num
   , datum
+  , uartConfig
   ) where
 
-import Ivory.Tower
-import Ivory.Language
+import           Ivory.Tower
+import           Ivory.Language
+import           Ivory.Artifact as R
+import qualified Tower.AADL     as A
+
+import           System.FilePath
+import qualified Paths_tower_camkes_odroid as P
 
 --------------------------------------------------------------------------------
--- Types
 
+-- Interface struct
 [ivory|
 struct uart_packet
-  { uint8_t uart_num
-  ; int64_t datum
+  { int32_t uart_num
+  ; uint8_t   datum
   }
 |]
 
---------------------------------------------------------------------------------
+uart_monitor :: String
+uart_monitor = "uart_monitor"
 
--- | Wrapper monitor
-uartMonitor
+-- | Wrapper monitor. The string names (e.g., "uart_monitor") must be unique and
+-- fixed.
+uartTower
   :: ChanOutput (Struct "uart_packet")
+  -- ^ Output from client
   -> ChanInput  (Struct "uart_packet")
+  -- ^ Input to client
   -> Tower e ()
-uartMonitor
-  s2wRx
-  -- ^ sender to wrapper (rx end)
-  w2rTx
-  -- ^ wrapper to receiver (tx end)
+uartTower s2wRx w2rTx
   = do
+  towerModule  uartModule
+  towerDepends uartModule
+
   -- From wrapper to driver
   (w2dTx, _ ) <- channel
   -- From driver to wrapper
   (_, d2wRx :: ChanOutput (Struct "uart_packet")) <- channel
 
-  externalMonitor "uart_monitor" $ do
+  externalMonitor uart_monitor $ do
     -- Rx from sender, define a symbol for handling msg, but don't implement it.
     handler s2wRx "send" $ do
       e <- emitter w2dTx 1
@@ -64,3 +72,31 @@ uartMonitor
     handler d2wRx "recv" $ do
       e <- emitter w2rTx 1
       callback $ \msg -> emit e msg
+
+--------------------------------------------------------------------------------
+
+uartConfig :: A.Config
+uartConfig = A.initialConfig
+  { A.configSystemHW  = A.ODROID
+  , A.configSystemOS  = A.CAmkES
+  , A.configArtifacts = uartArtifacts
+  }
+
+uartArtifacts :: [R.Artifact]
+uartArtifacts =
+  [ a compDir (uart_monitor <.> "camkes")
+  , a srcDir  "driver.c"
+  , a srcDir  ("smaccm_uart_monitor" <.> "c")
+  , a intfDir (uart_monitor <.> "idl4")
+  ]
+  where
+  a d f   = R.artifactPath d
+          $ R.artifactCabalFile P.getDataDir (uartDir </> d </> f)
+  uartDir = "data/uart"
+  compDir = "components" </> uart_monitor
+  srcDir  = compDir </> "src"
+  intfDir = "interfaces"
+
+uartModule :: Module
+uartModule = package "towerUartDeps" $
+  defStruct (Proxy :: Proxy "uart_packet")
---
+++ test/SerialTest.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PostfixOperators #-}
+
+--------------------------------------------------------------------------------
+-- UART client example, corresponding to
+-- smaccm/models/Trusted_Build_Test/test_uart_active2.
+
+--
+-- (c) 2015 Galois, Inc.
+--
+--------------------------------------------------------------------------------
+
+module Main where
+
+import Ivory.Language
+import Ivory.Stdlib
+import Ivory.Tower
+import Tower.AADL
+import Tower.Odroid.Serial
+
+testSerial :: Tower e ()
+testSerial = do
+  towerModule  towerDepModule
+  towerDepends towerDepModule
+
+  per <- period (2000`ms`)
+
+  -- From user code sender to wrapper
+  (s2wTx, s2wRx) <- channel
+  -- From wrapper to user code receiver
+  (w2rTx, w2rRx) <- channel
+
+  -- Driver wrapper
+  uartTower s2wRx w2rTx
+
+  monitor "sender" $ do
+    c <- stateInit "charState" (ival 65) -- 'A'
+    packet <- stateInit "packet" izero
+    handler per "periodicHandler" $ do
+      e <- emitter s2wTx 1 -- Send to wrapper
+      callback $ \_msg -> do
+        c' <- deref c
+        call_ send packet c'
+        emit e (constRef packet)
+        ifte_ (c' >? 90) -- 'Z'
+              (store c 65)
+              (c += 1)
+
+  monitor "receiver" $ do
+    handler w2rRx "receiverHandler" $ do
+      callback $ \msg -> do -- Receive from wrapper
+        call_ receive msg
+
+-- user_sender.c
+send :: Def('[Ref s (Struct "uart_packet"), Uint8] :-> ())
+send = proc "send" $ \packet c -> body $ do
+  store (packet ~> uart_num) 0
+  store (packet ~> datum) c
+  d <- deref (packet ~> datum)
+  call_ printf2 "Sending code: 0x%x --> %c\n" d d
+
+-- user_receiver.c
+receive :: Def('[ConstRef s (Struct "uart_packet")] :-> ())
+receive = proc "receive" $ \input1 -> body $ do
+  d <- input1 ~>* datum
+  call_ printf1 "Received input: 0x%x --> %c\n" d
+
+--------------------------------------------------------------------------------
+-- Compiler
+
+main :: IO ()
+main =
+  runCompileAADL
+    initialOpts { genDirOpts = Just "out/testUart"
+                , configOpts = uartConfig
+                }
+    testSerial
+
+--------------------------------------------------------------------------------
+-- Helpers
+
+[ivory|
+import (stdio.h, printf) void printf1(string x, uint8_t y)
+import (stdio.h, printf) void printf2(string x, uint8_t y, uint8_t z)
+|]
+
+towerDepModule :: Module
+towerDepModule = package "towerDeps" $ do
+  incl printf1
+  incl printf2
+  incl send
+  incl receive
