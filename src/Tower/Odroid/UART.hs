7f25525329c56151e6802111c089627db18ff0cf
leepike <leepike@gmail.com>
2015-05-27 16:31:10 -0400

    Revert "uart: make an intermediate monitor."
    Red-herring commit base on Ramses bug.
    
    This reverts commit 8964328e13b223f39b29ecd41dff45c9be0b47cf.

--- src/Tower/Odroid/UART.hs
+++ src/Tower/Odroid/UART.hs
@@ -51,39 +51,20 @@ uartTower
   towerDepends uartModule
   mapM_ towerArtifact uartArtifacts
 
-  -- From sender to translator
+  -- From sender to wrapper
   req_chan  <- channel
-  -- From translator to wrapper
-  req_chan' <- channel
   -- Response
   resp_chan <- channel
   -- Received byte
   rx_chan   <- channel
 
-  -- XXX We make a new monitor since if we just make this a handler in the
-  -- external monitor, then the only channel with the type of an
-  -- "ivory_string_UartPacket" is the outbound channel to the driver. But in
-  -- Tower, channels going nowhere are dropped, so we can't collect its type.
-  monitor "send_transdata" $ do
-    -- Now just pass through values from driver.
-    handler (snd req_chan) "send_translate" $ do
-      e <- emitter (fst req_chan') 1
-      callback $ \msg -> do
-        msg' <- local (izero :: Init UartPacket)
-        let srccap = arrayLen (msg ~> stringDataL)
-        srclen <- msg ~>* stringLengthL
-        assert $ srclen >=? 0 .&& srclen <=? srccap
-        assert $ srccap <=? arrayLen (msg' ~> stringDataL)
-        arrayCopy (msg' ~> stringDataL) (msg ~> stringDataL) 0 srclen
-        store (msg' ~> stringLengthL) srclen
-        emit e $ constRef msg'
-
-  wrapperMonitor (snd req_chan') (fst resp_chan) (fst rx_chan)
+  wrapperMonitor (snd req_chan) (fst resp_chan) (fst rx_chan)
 
   return (I.BackpressureTransmit (fst req_chan) (snd resp_chan), snd rx_chan)
 
 -- The wrapper just passes the channel values through to and from the driver.
-wrapperMonitor :: ChanOutput (Struct "ivory_string_UartPacket")
+wrapperMonitor :: IvoryString str
+               => ChanOutput str
                -> ChanInput (Stored IBool)
                -> ChanInput (Stored Uint8)
                -> Tower e ()
@@ -102,7 +83,15 @@ wrapperMonitor req_chanRx resp_chanTx rx_chanTx = do
     -- Now just pass through values from driver.
     handler req_chanRx "send" $ do
       e <- emitter (fst req_chan) 1
-      callback $ \msg -> emit e msg
+      callback $ \msg -> do
+        msg' <- local (izero :: Init UartPacket)
+        let srccap = arrayLen (msg ~> stringDataL)
+        srclen <- msg ~>* stringLengthL
+        assert $ srclen >=? 0 .&& srclen <=? srccap
+        assert $ srccap <=? arrayLen (msg' ~> stringDataL)
+        arrayCopy (msg' ~> stringDataL) (msg ~> stringDataL) 0 srclen
+        store (msg' ~> stringLengthL) srclen
+        emit e $ constRef msg'
 
     handler (snd rx_chan) "recv_rx" $ do
       e <- emitter rx_chanTx 1
